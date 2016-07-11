68fb29f93b57e1fa35e02c8e44ccc9ef2a40fff4
James Cabauatan <icyjms@yahoo.com>
2016-07-11 18:54:11 +0800

    Modify the about us log

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ var middleware = {
 app.use(middleware.logger);
 
 app.get('/about', middleware.requireAuthentication, function (req, res) {
-	res.send('About us!');
+	res.send('About us!!');
 });
 
 app.use(express.static(__dirname + '/public'));
